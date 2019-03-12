449a27c77fac41db7d950e5847ffb3efcac6f97f
Danilo <danilo.paixao@gingaone.com>
2019-03-12 17:06:20 -0300

    init repo

---
+++ src/com/danilopaixao/algorithm/sort/InsertionSort.java
@@ -0,0 +1,43 @@
+package com.danilopaixao.algorithm.sort;
+
+import com.danilopaixao.algorithm.Product;
+
+/**
+ * Insertion sort iterates, consuming one input element each repetition, 
+ * and growing a sorted output list. 
+ * At each iteration, insertion sort removes one element from the input data, 
+ * finds the location it belongs within the sorted list, and inserts it there. 
+ * It repeats until no input elements remain. 
+ * 
+ * Time complexity: quadratic O(n^2)
+ * 
+ * @author user
+ *
+ */
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		Product[] products = Product.generateProducts();
+		Product.printAllProducts(products);
+		products = order(products, products.length);
+		Product.printAllProducts(products);
+	}
+	
+	public static Product[] order(Product[] products, int quantity) {
+		for (int current = 0; current < quantity; current++) {
+			int analyse = current;
+			while(analyse > 0 && products[analyse].getPrice().compareTo(products[analyse-1].getPrice()) == -1) {
+				changePosition(products, analyse, analyse-1);
+				analyse--; //analyse goes till zero
+			}
+		}
+		return products;
+	}
+
+	public static void changePosition(Product[] products, int first, int second) {
+		Product firstProduct = products[first];
+		Product secondProduct = products[second];
+		products[first] = secondProduct;
+		products[second] = firstProduct;
+	}
+}
---
+++ src/com/danilopaixao/algorithm/sort/SelectionSort.java
@@ -0,0 +1,55 @@
+package com.danilopaixao.algorithm.sort;
+
+import com.danilopaixao.algorithm.Product;
+
+/**
+ * In computer science, selection sort is a sorting algorithm, specifically an in-place comparison sort. 
+ * It has O(n2) time complexity, making it inefficient on large lists, 
+ * and generally performs worse than the similar insertion sort. 
+ * Selection sort is noted for its simplicity, 
+ * and it has performance advantages over more complicated algorithms in certain situations, 
+ * particularly where auxiliary memory is limited.
+ * 
+ * Time complexity: quadratic O(n^2)
+ * 
+ * @author user
+ *
+ */
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		Product[] products = Product.generateProducts();
+		
+		int less = getPosLessPrice(products, 0, products.length-1);
+		System.out.println("Menor Preco >>");
+		Product.printProduct(products[less]);
+		System.out.println(">>");
+
+		Product.printAllProducts(products);
+		System.out.println(" ");
+		products = orderByLessPrice(products, 0, products.length);
+		Product.printAllProducts(products);
+	}
+	
+	public static Product[] orderByLessPrice(Product[] products, int ini, int end) {
+		for (int current = ini; current < end; current++) {
+			int less = getPosLessPrice(products, current, end-1);
+			Product productCurrent = products[current];
+			Product produtoLessPrice = products[less];
+			products[current] = produtoLessPrice;
+			products[less] = productCurrent;
+			
+		}
+		return products;
+	}
+
+	public static int getPosLessPrice(Product[] products, int posInitial, int posFinal) {
+		int posLessPrice = posInitial;
+		for (int atual = posInitial; atual <= posFinal; atual++) {
+			if (products[atual].getPrice().compareTo(products[posLessPrice].getPrice()) == -1)
+				posLessPrice = atual;
+		}
+		return posLessPrice;
+	}
+
+}
