500bf964194e6036af220db79f58b84c36ca04f6
DaniloPaixaoJesus <contato.danilo.paixao@gmail.com>
2019-03-19 09:31:50 -0300

    algorithm studies - sort algorithm

---
+++ src/com/danilopaixao/algorithm/search/Binary.java
@@ -0,0 +1,16 @@
+package com.danilopaixao.algorithm.search;
+
+/**
+ * Binary search works on sorted arrays. 
+ * Binary search begins by comparing the middle element of the array with the target value. 
+ * If the target value matches the middle element, its position in the array is returned. 
+ * If the target value is less than the middle element, the search continues in the lower half of the array. 
+ * If the target value is greater than the middle element, the search continues in the upper half of the array. 
+ * By doing this, the algorithm eliminates the half in which the target value cannot lie in each iteration
+ * 
+ * @author user
+ *
+ */
+public class Binary {
+
+}
