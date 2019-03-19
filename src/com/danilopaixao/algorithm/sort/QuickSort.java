824bc7ad9529a73a61513aa77d2b796ea8aec77c
DaniloPaixaoJesus <contato.danilo.paixao@gmail.com>
2019-03-18 23:26:12 -0300

    algorithm studies - sort algorithm

--- src/com/danilopaixao/algorithm/sort/QuickSort.java
+++ src/com/danilopaixao/algorithm/sort/QuickSort.java
@@ -4,6 +4,14 @@ import com.danilopaixao.algorithm.Note;
 
 /**
  * 
+ * Quicksort is a comparison sort, meaning that it can sort items of any type for which a "less-than" relation (formally, a total order) is defined. 
+ * In efficient implementations it is not a stable sort, meaning that the relative order of equal sort items is not preserved. 
+ * Quicksort can operate in-place on an array, requiring small additional amounts of memory to perform the sorting. 
+ * It is very similar to selection sort, except that it does not always choose worst-case partition.
+ * 
+ * Mathematical analysis of quicksort shows that, on average, the algorithm takes O(n log n) comparisons to sort n items. 
+ * In the worst case, it makes O(n2) comparisons, though this behavior is rare. 
+ * 
  * Time complexity: Logarithmic O(NlogN)
  * 
  * @author user
